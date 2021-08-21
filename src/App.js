00fd445f6743902af0b43f9c20205db15c727702
Dann Stockton <dannstockton@gmail.com>
2021-08-20 19:53:49 -0600

    Fade in, fade out :feelsgoodman:

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import {useEffect, useState} from "react";
 function App() {
     const icebreakers = useIceBreakers()
     const [ib, setIB] = useState("")
+    const [hidden, setHidden] = useState(false)
     useEffect(() => {
         setIB(icebreakers.next())
     }, [])
+
+    const onClick = () => {
+        setHidden(true)
+        setTimeout(() => {
+            setIB(icebreakers.next())
+            setHidden(false)
+        }, 1000)
+    }
+
     return (
         <div className="App">
             <div className={'justify-center grid align-middle text-center'}>
                 <div className={'my-12 max-w-xl'}>
-                    <p className={'text-5xl text-gray-200'}>{ib.text}</p>
+                    <p className={`text-5xl text-gray-200 transition duration-700 ease-in-out ${hidden && 'text-opacity-0'}`}>{ib.text}</p>
                 </div>
-                <button onClick={() => setIB(icebreakers.next())} className={'px-6 bg-red-500 h-14 rounded-2xl text-4xl text-gray-200 shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50'}>Next</button>
+                <button onClick={onClick} className={'px-6 bg-red-500 h-14 rounded-2xl text-4xl text-gray-200 shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50'}>Next</button>
             </div>
         </div>
     );
